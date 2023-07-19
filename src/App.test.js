774d4e2e06fb9551dbf6ed804b3270439af761f3
man7788 <jerman1841@gmail.com>
2023-07-20 07:00:23 +0800

    Add 1 more test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
+/* eslint-disable testing-library/no-container, testing-library/no-node-access */
+
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
+import { act } from 'react-dom/test-utils';
 
-// Clock
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
+});
+
 // User click display dropdown menu
 it('should display dropdown menu when click on photo', async () => {
   const user = userEvent.setup();
@@ -71,7 +82,6 @@ it('should hide dropdown menu when select incorrect menu name from target click'
 
   const { container } = render(<App />);
 
-  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
   const target = container.getElementsByClassName('Target');
   const box = screen.getByTestId('box');
   const menu = screen.getByTestId('menu');
@@ -88,15 +98,13 @@ it('should hide dropdown menu when select incorrect menu name from target click'
   expect(boxStyles.display).toBe('none');
   expect(menuStyles.display).toBe('none');
 });
-// User click target select correct menu name will change photo
 
-describe.only('Click on target and select correct name', () => {
+describe('Click on target and select correct name', () => {
   it('should hide dropdown menu', async () => {
     const user = userEvent.setup();
 
     const { container } = render(<App />);
 
-    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
     const target = container.getElementsByClassName('Target');
     const box = screen.getByTestId('box');
     const menu = screen.getByTestId('menu');
@@ -119,9 +127,7 @@ describe.only('Click on target and select correct name', () => {
 
     const { container } = render(<App />);
 
-    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
     const target = container.getElementsByClassName('Target');
-    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
     const frames = container.getElementsByClassName('Photo');
 
     await waitFor(async () => await user.click(target[0].childNodes[0]));
@@ -136,8 +142,41 @@ describe.only('Click on target and select correct name', () => {
     expect(frameStyles.color).toBe('lightgrey');
   });
 });
-// Clock should stop when gameover
+
 // Popup screen when gameover
-// Popup scree should display finish time
-// Input should display user type in
-// Input should reset after submit score
+it('Pop up screen should popup when gameover and display score time', async () => {
+  const user = userEvent.setup();
+
+  const { container } = render(<App />);
+
+  // https://legacy.reactjs.org/docs/testing-recipes.html#timers
+  act(() => {
+    jest.runAllTimers();
+  });
+
+  const target = container.getElementsByClassName('Target');
+  const menuNames = screen.getAllByRole('listitem', { hidden: true });
+  const clock = container.getElementsByClassName('Clock');
+  const popup = container.getElementsByClassName('Popup');
+  const popupHeader = container.getElementsByClassName('header');
+
+  await waitFor(async () => await user.click(target[0].childNodes[0]));
+  await waitFor(async () => await user.click(menuNames[0]));
+
+  await waitFor(async () => await user.click(target[1].childNodes[0]));
+  await waitFor(async () => await user.click(menuNames[1]));
+
+  await waitFor(async () => await user.click(target[2].childNodes[0]));
+  await waitFor(async () => await user.click(menuNames[2]));
+
+  act(() => {
+    jest.runAllTimers();
+  });
+
+  const popupStyles = getComputedStyle(popup[0]);
+  const clockTime = clock[0].textContent;
+  const scoreTime = popupHeader[0].childNodes[2].textContent;
+
+  expect(popupStyles.display).toBe('flex');
+  expect(scoreTime).toMatch(clockTime);
+});
